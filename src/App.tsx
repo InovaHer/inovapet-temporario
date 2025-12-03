a227b6e065a83ed12a3eca43b2295d3c10d00244
Patricia Eliseu <patyeliseu4@gmail.com>
2025-12-03 12:15:14 -0300

    add usuario login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
 
+import { AuthProvider } from "./contexts/AuthContext"
 
-import './App.css'
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
+import Perfil from "./pages/perfil/Perfil"
+import AtualizarPerfil from "./pages/perfil/AtualizarPerfil"
 
 function App() {
   
 
   return (
-    <>
-    
-     <div></div>
+    <>    
+     <AuthProvider>
+      <ToastContainer />
+      <BrowserRouter>
+        {/* <Navbar /> */}
+          <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+						
+						<Route path="/cadastro" element={<Cadastro />} />
+						
+            <Route path="/perfil" element={<Perfil />} />
+            <Route path="/atualizarusuario" element={<AtualizarPerfil />} />
+          </Routes>
+          </div>
+        {/* <Footer/> */}
+      </BrowserRouter>
+    </AuthProvider>
     </>
   )
 }
