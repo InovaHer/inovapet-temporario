f7972e55196a67703aafb8ff5222bc1b09e93434
Jamila Moraes Cardoso <jmc18.ads@gmail.com>
2025-12-05 10:04:25 -0300

    Deploy do Projeto InovaPet

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Instância do Axios
 const api = axios.create({
-    baseURL: 'https://inovaher-projeto-deliverypet.onrender.com/'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 // Função para Cadastrar Usuário
