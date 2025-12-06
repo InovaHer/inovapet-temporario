d4de6632634f251b82e64155b3bfbaeee1c0e89c
Marcy_ <marciagisseli21@gmail.com>
2025-12-05 23:08:12 -0300

    Comentando o linting para o deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Instância do Axios
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL336
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 // Função para Cadastrar Usuário
